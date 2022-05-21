ea204ca18c135a6775df9d9a48602f91f10c7bf3
Driss Alaoui <sulimani.driss.alaui@nure.ua>
2022-05-21 18:12:18 +0100

    Adding A Status Bar

--- Viewer.py
+++ Viewer.py
@@ -15,9 +15,9 @@ my_img4= ImageTk.PhotoImage(Image.open("images/img4.jpg"))
 my_img5= ImageTk.PhotoImage(Image.open("images/img5.jpg"))
 
 image_list=[my_img1, my_img2, my_img3,my_img4,my_img5]
-
-
 index = 3
+
+status = Label(root,text="Image N° "+str(index+1)+" of "+str(len(image_list)), bd=5,fg="Blue", relief=SUNKEN, anchor=E)
 
 my_label = Label(image=image_list[index])
 my_label.grid( row=0, column=0, columnspan=3)
@@ -26,6 +26,7 @@ def back():
     global index
     global image_list
     global my_label
+    global status
     if(index == 0):
         index=4
     else:
@@ -33,12 +34,15 @@ def back():
     my_label.grid_forget()
     my_label = Label(image=image_list[index])
     my_label.grid(row=0, column=0, columnspan=3)
-
+    status = Label(root, text="Image N° " + str(index+1) + " of " + str(len(image_list)), bd=5, fg="Blue", relief=SUNKEN,
+                   anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
 
 def forwar():
     global index
     global image_list
     global my_label
+    global status
     if (index == 4):
         index = 0
     else:
@@ -47,7 +51,10 @@ def forwar():
     my_label.grid_forget()
     my_label = Label(image=image_list[index])
     my_label.grid(row=0, column=0, columnspan=3)
-
+    status = Label(root, text="Image N° " + str(index + 1) + " of " + str(len(image_list)), bd=5, fg="Blue",
+                   relief=SUNKEN,
+                   anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W + E)
 
 
 
@@ -57,6 +64,7 @@ button_forward = Button(root, text=">>", command=forwar)
 
 button_back.grid(row=1, column=0)
 button_quit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=30)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 root.mainloop()
